dde835ad7e16ea7dcfb5741341481cb1820e0716
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2025-08-13 01:04:33 +0600

    warning disabled
    (no layout shift feature is needed so didn't usenext/image )

--- src/components/organisms/PhoneMockup.tsx
+++ src/components/organisms/PhoneMockup.tsx
@@ -7,6 +7,7 @@ interface PhoneMockupProps {
 const PhoneMockup: React.FC<PhoneMockupProps> = ({ className = '' }) => {
     return (
         <div className={`${className}`}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
                 src="assets/images/mockup.png"
                 className="max-w-[302px] mx-auto h-[300px] lg:!max-w-[100%] lg:h-[660px]"
